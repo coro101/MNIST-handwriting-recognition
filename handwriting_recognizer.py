ca30f43527da3e1efb067cf26066e538b974cbef
Sangwoo <coro101@naver.com>
2018-02-20 18:54:48 +0900

    Add load mnist data function

--- handwriting_recognizer.py
+++ handwriting_recognizer.py
@@ -1,5 +1,29 @@
 # -*- coding: utf-8 -*-
 
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+def load_mnist(mnist_path='MNIST_data/'):
+    """
+    Load mnist data from tensorflow.
+
+    :param mnist_path: Path where the mnist data will be stored
+    :return:
+    mnist -- Tensorflow handwriting data
+        train - Flattened and normalized images of shape (55000, 784), width and height of a image is 28
+        test - Flattened and normalized images of shape (10000, 784), width and height of a image is 28
+        label - One hot encoded labels of shape (55000, 10)
+    """
+    mnist = input_data.read_data_sets(mnist_path, one_hot=True)
+
+    print('Number of training examples: ' + str(mnist.train.num_examples))
+    print('Number of test examples: ' + str(mnist.test.num_examples))
+
+    return mnist
+
 
 def main():
-    print('handwriting recognizer.')
+    mnist = load_mnist()
+
+
+main()
